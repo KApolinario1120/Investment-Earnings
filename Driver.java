0ffe57ce9790e940ae78ef79aec54deff2c44241
KApolinario1120 <kevinsquared@gmail.com>
2022-10-30 09:50:26 -0400

    Add driver class and scanner import

---
+++ Driver.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class Driver {
+
+    public  static void main(String[] args) {
+        Scanner scan =  new Scanner(System.in);
+        System.out.println("Enter your buying price per share: ");
+    }
+}
